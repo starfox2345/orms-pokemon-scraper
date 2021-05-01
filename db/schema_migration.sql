f90413438c350964825569cc58908f79830fbec3
yatsun <yatsunlam25@gmail.com>
2021-05-01 02:20:39 -0400

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,8 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+-- sqlite3 pokemon.db
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT
+);
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    # attr_reader :db
+
+    def initialize(options={})
+        options.each do |key, value|
+            self.send("#{key}=", value) if respond_to?("#{key}=")
+        end
+    end
+    # def initialize(id, name, type, db)
+    #     @id = id
+    #     @name = name
+    #     @type = type
+    #     @db = db
+    # end
+    def save
+        sql = <<-SQL
+            INSERT INTO pokemon(name, type) VALUES (?, ?)
+        SQL
+        
+        DB[:conn].execute(sql, self.name, self.type)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
 end
